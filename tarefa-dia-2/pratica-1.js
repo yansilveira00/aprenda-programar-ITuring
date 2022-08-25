bdbb6cc97c31ea8542e5b3f5a78de2c20dcc1c80
yansilveira00 <ysds@aluno.ifnmg.edu.br>
2022-08-25 19:16:11 -0300

    if and else

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,8 @@
-var nome;
-var email;
-var profissao;
-var idade;
-var local;
+let nome = "Yan"
+let email = "yan@gmail.com"
+let profissao = "programador"
+let idade = 21
+let localDosSonhos = "Disney"
+let mensagem = "Olá, " + nome + "! Somos da emprea Hurb. Estamos confirmando a compra do pacote de viagem para " + localDosSonhos + ". Confirme pra gente alguns dados? A sua idade é " + idade + ", você é" + profissao + " e seu email é " + email + "? Aguardamos sua resposta. A gente agradece a confiança e boa viagem! ;D"
+
+console.log(mensagem)
