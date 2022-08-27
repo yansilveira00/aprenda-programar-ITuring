e8f7e99e4e451c5b57f2d61444af5e77bfa8c8c5
yansilveira00 <ysds@aluno.ifnmg.edu.br>
2022-08-27 15:54:28 -0300

    pratica 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+
+
+
+
+let multiplicador = 9
+let n = 9 // multiplicar de 1 a n
+let i = 1
+
+while (i <= 10) {
+    let resultado = multiplicador * i
+    let textoExibicao =`${multiplicador} X ${i} = ${resultado}`
+    console.log(textoExibicao)
+    i++
+}
